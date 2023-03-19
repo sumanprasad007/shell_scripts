62c605f3bd0cea94f7ac996ce8cf7f75fb04caf3
Suman Prasad <sumanprasad9766@gmail.com>
2023-03-19 14:51:15 +0530

    modified the contents of calculator.sh file

--- calculator.sh
+++ calculator.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
-Testing the code
+# Function to perform addition
+add() {
+  result=$(($1 + $2))
+  echo "The result of $1 + $2 = $result"
+}
 
-Testing the branching strategy for the code
+# Function to perform subtraction
+subtract() {
+  result=$(($1 - $2))
+  echo "The result of $1 - $2 = $result"
+}
+
+# Function to perform multiplication
+multiply() {
+  result=$(($1 * $2))
+  echo "The result of $1 * $2 = $result"
+}
+
+# Function to perform division
+divide() {
+  result=$(($1 / $2))
+  echo "The result of $1 / $2 = $result"
+}
+
+# User interface
+echo "Simple Calculator"
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+echo "Enter the operator (+, -, *, /): "
+read operator
+
+# Switch case to perform the calculation based on the operator
+case $operator in
+  "+") add $num1 $num2
+    ;;
+  "-") subtract $num1 $num2
+    ;;
+  "*") multiply $num1 $num2
+    ;;
+  "/") divide $num1 $num2
+    ;;
+  *) echo "Invalid operator"
+    ;;
+esac
